1a2612cf52eab6cee0293163aee85a199299f7e8
Wander Souza <wanderdasouza@gmail.com>
2020-10-27 08:56:14 -0300

    Cria projeto nuxt.js

---
+++ components/system.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="quadro"></div>
+</template>
+
+<script>
+export default {
+  async fetch() {
+    this.systems = await fetch('http://localhost:8000/sistema').then((res) =>
+      res.json()
+    )
+  },
+  data() {
+    return {
+      systems: [],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.quadro {
+  display: flex;
+  flex-direction: column;
+  background-color: olivedrab;
+  width: 100%;
+  height: 700px;
+  padding: 20px;
+}
+.quadradinho {
+  background-color: red;
+  width: 50px;
+  height: 50px;
+  margin-right: 50px;
+}
+.gridSystem {
+  background-color: chocolate;
+  height: 100%;
+  width: 100%;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <system />
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import system from '../components/system.vue'
+
+export default {
+  components: {
+    system,
+    // Logo,
+    // VuetifyLogo,
+  },
+}
+</script>
